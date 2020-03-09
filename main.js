d07fb7a535ce34ae9062676a18d86bc8b105259b
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-09 14:55:15 +0100

    drawing grid

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 let resolution = 40;
 let cols;
 let rows;
+let grid;
 
 function setup() {
     createCanvas(600,400);
     cols = width / resolution;
     rows = height / resolution;
+    grid = create2dgrid(cols, rows);
+    populateGrid();
 }
 
+
 function draw() {
-    create2dgrid(cols, rows);
+    background(255);
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            if(grid[i][j] == 1) {
+                fill(51);
+                stroke(0);
+                rect(x, y, resolution, resolution);
+            }
+        }
+    }
+}
+
+function populateGrid() {
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2));
+        }
+    }
 }
 
 function create2dgrid(cols, rows) {
-    let grid = new Array(cols);
-    for (let col = 0; col < cols.length; col++) {
-        cols[col] = new Array(rows);
+    let arr = new Array(cols);
+    for (let col = 0; col < arr.length; col++) {
+        arr[col] = new Array(rows);
     }
-    return grid;
+    return arr;
 }
-
